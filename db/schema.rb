98dddc7e221ffd686078df131ce51343825cc83a
Rails Udemy Course <opacic.milos_95@hotmail.com>
2018-02-22 01:48:55 +0100

    add secure password and attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221145521) do
+ActiveRecord::Schema.define(version: 20180222003143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180221145521) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
